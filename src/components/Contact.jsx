e40a77d82d0809306c37e29185c905ea44d4172d
gnguyen2 <gnguyen2@academic.rrc.ca>
2025-01-27 20:52:58 -0600

    fix run build 3

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import AnimatedTitle from "./AnimatedTitle";
-import Button from "./Button";
+import Button from "./button";
 
 const ImageClipBox = ({ src, clipClass }) => (
   <div className={clipClass}>
